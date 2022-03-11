d98384b6457531edf60ea871e74a0e09206af469
Mikko Suniala <mikko.suniala@iki.fi>
2022-03-11 14:53:25 +0200

    Lisää yksinkertainen OpenLayers-kartta

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import 'ol/ol.css';
+import OSM from 'ol/source/OSM';
+import { useEffect } from 'react';
 import './App.css';
+
+function Kartta() {
+  useEffect(() => {
+    new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: 'kartta',
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    })
+  })
+
+  return <div id="kartta"/>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Kartta />
     </div>
   );
 }
