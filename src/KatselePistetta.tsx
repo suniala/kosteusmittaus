6efaaf903895803af2b29efb4f2f919282f040cc
Mikko Suniala <mikko.suniala@iki.fi>
2022-04-21 20:28:24 +0300

    Kokeilu tavoista toteuttaa yleiskäyttöinen "lataava" komponentti

--- src/KatselePistetta.tsx
+++ src/KatselePistetta.tsx
@@ -1,13 +1,20 @@
-import { eitherFold } from './functional/either'
-import { optGet } from './functional/option'
-import { noudaPiste } from './palvelin/palvelin'
 import { usePromise } from './apu/hook'
+import { Lataava1, Lataava2 } from './Lataava'
+import { noudaPiste } from './palvelin/palvelin'
 import { Mittauspiste } from './yhteiset'
 
-interface TiedotProps {
+interface Tiedot1Props {
   piste: Mittauspiste
 }
-const Tiedot = ({ piste }: TiedotProps) => (
+
+const Tiedot1 = ({ piste }: Tiedot1Props) => (
+  <ul>
+    <li>{piste.id}</li>
+    <li>{piste.nimi}</li>
+  </ul>
+)
+
+const Tiedot2 = (piste: Mittauspiste) => (
   <ul>
     <li>{piste.id}</li>
     <li>{piste.nimi}</li>
@@ -18,20 +25,29 @@ export interface KatselePistettaProps {
   valittuPiste: string
 }
 
+
 export const KatselePistetta = (props: KatselePistettaProps) => {
   const { lataa, ehkaTulos } = usePromise(() => noudaPiste(props.valittuPiste), [props.valittuPiste])
 
   return (
     <div>
-      {
-        lataa
-          ? (<span>lataa</span>)
-          : eitherFold(
-            optGet(ehkaTulos),
-            virhe => (<span>virhe: {virhe}</span>),
-            (tulos) => (<Tiedot piste={tulos} />)
-          )
-      }
+      <span>Tässä on kokeilumielessä muutama erilainen tapa käyttää Lataava-komponettia</span>
+      <div>
+        <Lataava1 lataa={lataa} ehkaTulos={ehkaTulos} luoLapsi={(tulos: Mittauspiste) => (<Tiedot1 piste={tulos} />)} />
+      </div>
+      <div>
+        <Lataava2 lataa={lataa} ehkaTulos={ehkaTulos}>
+          {tulos => (<Tiedot1 piste={tulos} />)}
+        </Lataava2>
+      </div>
+      <div>
+        <Lataava2 lataa={lataa} ehkaTulos={ehkaTulos}>
+          {Tiedot2}
+        </Lataava2>
+      </div>
+      <div>
+        <Lataava2 lataa={lataa} ehkaTulos={ehkaTulos} children={Tiedot2} />
+      </div>
     </div>
   )
 }
